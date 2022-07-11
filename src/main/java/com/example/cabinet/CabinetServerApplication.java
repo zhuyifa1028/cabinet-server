921a12a8a7040137b7ac18b64f595447efa0b8a6
留行亭长丶 <zhuyifa1028@gmail.com>
2022-07-11 20:12:03 +0800

    spring boot项目

---
+++ src/main/java/com/example/cabinet/CabinetServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cabinet/CabinetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
