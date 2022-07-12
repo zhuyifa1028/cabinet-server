d23f165dce701e215e89ba8086f97cac28fa628e
留行亭长丶 <zhuyifa1028@gmail.com>
2022-07-12 08:58:09 +0800

    修改boot版本为2.7.1

--- src/test/java/com/example/cabinet/CabinetApplicationTests.java
+++ src/test/java/com/example/cabinet/CabinetApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CabinetServerApplicationTests {
+class CabinetApplicationTests {
 
     @Test
     void contextLoads() {
